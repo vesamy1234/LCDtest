ca6749651dc4b6cefac038b3ca1f818c4fd84cf7
vesamy1234 <vesamy123@gmail.com>
2024-10-05 20:48:47 +0700

    n

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -157,58 +157,58 @@ uint8_t Keypad_Getkey()
 }
 
 // A utility function to reverse a string
-void reverse(char str[], int length)
-{
-    int start = 0;
-    int end = length - 1;
-    while (start < end) {
-        char temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        end--;
-        start++;
-    }
-}
-// Implementation of citoa()
-char* citoa(int64_t num, char* str, int base)
-{
-    int i = 0;
-    bool isNegative = false;
-
-    /* Handle 0 explicitly, otherwise empty string is
-     * printed for 0 */
-    if (num == 0) {
-        str[i++] = '0';
-        str[i] = '\0';
-        return str;
-    }
-
-    // In standard itoa(), negative numbers are handled
-    // only with base 10. Otherwise numbers are
-    // considered unsigned.
-    if (num < 0 && base == 10) {
-        isNegative = true;
-        num = -num;
-    }
-
-    // Process individual digits
-    while (num != 0) {
-        int rem = num % base;
-        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-        num = num / base;
-    }
-
-    // If number is negative, append '-'
-    if (isNegative)
-        str[i++] = '-';
-
-    str[i] = '\0'; // Append string terminator
-
-    // Reverse the string
-    reverse(str, i);
-
-    return str;
-}
+//void reverse(char str[], int length)
+//{
+//    int start = 0;
+//    int end = length - 1;
+//    while (start < end) {
+//        char temp = str[start];
+//        str[start] = str[end];
+//        str[end] = temp;
+//        end--;
+//        start++;
+//    }
+//}
+//// Implementation of citoa()
+//char* citoa(int64_t num, char* str, int base)
+//{
+//    int i = 0;
+//    bool isNegative = false;
+//
+//    /* Handle 0 explicitly, otherwise empty string is
+//     * printed for 0 */
+//    if (num == 0) {
+//        str[i++] = '0';
+//        str[i] = '\0';
+//        return str;
+//    }
+//
+//    // In standard itoa(), negative numbers are handled
+//    // only with base 10. Otherwise numbers are
+//    // considered unsigned.
+//    if (num < 0 && base == 10) {
+//        isNegative = true;
+//        num = -num;
+//    }
+//
+//    // Process individual digits
+//    while (num != 0) {
+//        int rem = num % base;
+//        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+//        num = num / base;
+//    }
+//
+//    // If number is negative, append '-'
+//    if (isNegative)
+//        str[i++] = '-';
+//
+//    str[i] = '\0'; // Append string terminator
+//
+//    // Reverse the string
+//    reverse(str, i);
+//
+//    return str;
+//}
 
 /* USER CODE END 0 */
 
@@ -320,10 +320,7 @@ int main(void)
 
     		}
     		Lcd_cursor(&lcd, 1, 0);
-//    		char snum[16];
-//    		citoa(kq, snum, 10);
     		Lcd_int(&lcd, kq);
-//    		Lcd_string(&lcd, snum);
     	}
     	if (key==DEL)
     	{
